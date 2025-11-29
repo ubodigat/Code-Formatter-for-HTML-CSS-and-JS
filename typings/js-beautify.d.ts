5996359e6625a7fd1aa6a1bea3ce0aced7ecaa07
ubodigat <kontakt@ubodigat.com>
2025-11-29 06:05:36 +0100

    29.11.2025 | Projektdateien aktualisiert

--- typings/js-beautify.d.ts
+++ typings/js-beautify.d.ts
@@ -1,11 +1,11 @@
 declare module "js-beautify" {
 	
 	interface options {
-		indent_size?: number; // (4) — indentation size,
-		indent_char?: string; // (space) — character to indent with,
-		selector_separator_newline?: boolean; // (true) - separate selectors with newline or not (e.g. "a,\nbr" or "a, br")
-		end_with_newline?: boolean; // (false) - end with a newline
-		newline_between_rules?: boolean; // (true) - add a new line after every css rule
+		indent_size?: number;
+		indent_char?: string;
+		selector_separator_newline?: boolean;
+		end_with_newline?: boolean;
+		newline_between_rules?: boolean;
 	}
 	
 	interface beautifyCSS {
